edad6a245799656465581c5c04e39fdb643125a5
BominRahmani <bominrahmani@gmail.com>
2023-11-30 16:50:36 -0500

    initial sample app addition

---
+++ active-directory/network-variables.tf
@@ -0,0 +1,5 @@
+variable "network-subnet-cidr" {
+  type        = string
+  description = "The CIDR for the network subnet"
+  default     = "10.10.15.0/24"
+}
---
+++ active-directory/windows-versions.tf
@@ -0,0 +1,6 @@
+variable "windows_2019_sku" {
+  type        = string
+  description = "SKU for Windows Server 2019"
+  default     = "windows-cloud/windows-2019"
+}
+
