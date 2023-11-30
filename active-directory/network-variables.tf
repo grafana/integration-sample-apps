cb81ec2589a9777cdb65aff26f0a3804f22f048d
BominRahmani <bominrahmani@gmail.com>
2023-11-30 16:55:36 -0500

    remove accidental push to main

--- active-directory/network-variables.tf
+++
@@ -1,5 +0,0 @@
-variable "network-subnet-cidr" {
-  type        = string
-  description = "The CIDR for the network subnet"
-  default     = "10.10.15.0/24"
-}
--- active-directory/windows-vm-variables.tf
+++
@@ -1,5 +0,0 @@
-variable "windows_instance_type" {
-  type        = string
-  description = "VM instance type for Windows Server"
-  default     = "n2-standard-2"
-}
