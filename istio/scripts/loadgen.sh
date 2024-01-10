8da558a9face6c1aba95f22a296e5b538ade8fab
Stefan Kurek <stefan.kurek@observiq.com>
2024-01-10 10:15:19 -0500

    Istio sample app now installs Grafana Agent and sends metrics

--- istio/scripts/loadgen.sh
+++ istio/scripts/loadgen.sh
@@ -4,12 +4,13 @@ export INGRESS_NAME=istio-ingress
 export INGRESS_NS=istio-ingress
 export INGRESS_HOST=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 export INGRESS_PORT=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
-export SECURE_INGRESS_PORT=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
-export TCP_INGRESS_PORT=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.spec.ports[?(@.name=="tcp")].port}')
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
 
+echo "Generating Load"
 while true; do
     curl -s -o /dev/null "http://$GATEWAY_URL/productpage"
     curl -s -o /dev/null "http://$GATEWAY_URL/productpage"
     curl -s -o /dev/null "http://$GATEWAY_URL/bad"
+    curl -s -o /dev/null "http://$GATEWAY_URL/api/v1/products/bad"
+    sleep 1
 done
