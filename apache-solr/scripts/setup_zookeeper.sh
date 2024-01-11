275660b7931ef2a2d7e4d11349d860bec8199bd3
Jonathan Wamsley <jonathan.wamsley@bluemedora.com>
2024-01-11 08:55:26 -0500

    add apache solr sample app

---
+++ apache-solr/scripts/setup_zookeeper.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+VM_ID=$1
+IP_FILE=$2
+
+# Read IP addresses from file
+readarray -t IPS < "${IP_FILE}"
+
+# Assign IPs to variables
+IP1=${IPS[0]}
+IP2=${IPS[1]}
+IP3=${IPS[2]}
+
+echo "Setting up ZooKeeper on VM_ID: $VM_ID"
+
+# Update and install dependencies
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install default-jdk zookeeperd netcat -y
+
+# Configure ZooKeeper
+ZOO_CFG="/etc/zookeeper/conf/zoo.cfg"
+sudo bash -c "echo server.1=$IP1:2888:3888 >> $ZOO_CFG"
+sudo bash -c "echo server.2=$IP2:2888:3888 >> $ZOO_CFG"
+sudo bash -c "echo server.3=$IP3:2888:3888 >> $ZOO_CFG"
+
+echo $VM_ID | sudo tee /var/lib/zookeeper/myid
+
+# Restart ZooKeeper
+sudo systemctl restart zookeeper
+
+# Wait a bit for ZooKeeper to stabilize
+sleep 10
