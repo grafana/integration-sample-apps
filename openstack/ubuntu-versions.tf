2892916a1699fce13934df1dacafbd7e1ab44fff
Caleb Hurshman <caleb@hurshman.com>
2024-02-22 14:41:53 -0500

    openstack sample app

---
+++ openstack/ubuntu-versions.tf
@@ -0,0 +1,5 @@
+variable "ubuntu_image" {
+  type        = string
+  description = "SKU for Ubuntu 22.04 LTS"
+  default     = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240208"
+}
