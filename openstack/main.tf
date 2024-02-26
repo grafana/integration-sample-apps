9629fdae6c135f17703bf75bfe5c1036e3e42b54
Caleb Hurshman <caleb@hurshman.com>
2024-02-26 16:33:11 -0500

    fmt, remove unnecessary files

--- openstack/main.tf
+++ openstack/main.tf
@@ -12,14 +12,14 @@ resource "google_compute_instance" "vm_instance_public" {
   }
   metadata_startup_script = templatefile("${path.module}/bootstrap-vm.tp1", {
     # If you have variables that you pass to the template, they go here
-    "loadgen"  = var.loadgen_script
-    "config"   = var.agent_config
-    "loki_username" = var.loki_username
-    "loki_password" = var.loki_password
-    "loki_url" = var.loki_url
+    "loadgen"             = var.loadgen_script
+    "config"              = var.agent_config
+    "loki_username"       = var.loki_username
+    "loki_password"       = var.loki_password
+    "loki_url"            = var.loki_url
     "prometheus_username" = var.prometheus_username
     "prometheus_password" = var.prometheus_password
-    "prometheus_url" = var.prometheus_url
+    "prometheus_url"      = var.prometheus_url
   })
   network_interface {
     network    = google_compute_network.vpc.name
