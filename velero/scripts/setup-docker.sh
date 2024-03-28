e7ce6a8796828876896670717d2574fd777e9638
BominRahmani <bominrahmani@gmail.com>
2024-03-28 15:58:06 -0400

    script changes

--- velero/scripts/setup-docker.sh
+++ velero/scripts/setup-docker.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
