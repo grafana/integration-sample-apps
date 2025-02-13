3bf5a95069e233ae4e1ad1b671d84ad6f53039f4
Emily Rager <emily.rager@grafana.com>
2025-02-13 11:31:58 +0100

    Rename .river filetypes to .alloy

--- quickstart/sample-apps/k3s-singlenode/scripts/install_monitoring.sh
+++ quickstart/sample-apps/k3s-singlenode/scripts/install_monitoring.sh
@@ -8,4 +8,4 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # Install k8s monitoring chart with `extraConfig` for the suo (Pinned to v1 helm chart)
-helm install k8s-monitoring grafana/k8s-monitoring -n monitoring --create-namespace --wait --values monitoring.yaml --set-file extraConfig=configs/suo.river --version ^1
+helm install k8s-monitoring grafana/k8s-monitoring -n monitoring --create-namespace --wait --values monitoring.yaml --set-file extraConfig=configs/suo.alloy --version ^1
--- sample-apps/kafka/scripts/install_monitoring.sh
+++ sample-apps/kafka/scripts/install_monitoring.sh
@@ -8,4 +8,4 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # Install k8s monitoring chart with `extraConfig` for kafka (Pinned to v1 helm chart)
-helm install k8s-monitoring grafana/k8s-monitoring -n monitoring --create-namespace --wait --values monitoring.yaml --set-file extraConfig=configs/kafka.river --version ^1
+helm install k8s-monitoring grafana/k8s-monitoring -n monitoring --create-namespace --wait --values monitoring.yaml --set-file extraConfig=configs/kafka.alloy --version ^1
