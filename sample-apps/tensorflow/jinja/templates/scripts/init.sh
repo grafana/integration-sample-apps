e809ea7bef2ced8e086aa2d6da236f252e4b6b01
schmikei <keith.schmitty@gmail.com>
2025-06-03 13:28:05 -0400

    remove breaking commetn in script file

--- sample-apps/tensorflow/jinja/templates/scripts/init.sh
+++ sample-apps/tensorflow/jinja/templates/scripts/init.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Run TensorFlow serving image
+# Custom image with baked in model, see https://github.com/observIQ/application-helm-charts/tree/main/container/tensorflow
 docker run -d --platform linux/amd64 -p 8501:8501 --name tensorflow-serving --rm \
   -e MODEL_NAME=half_plus_two \
   -v /home/ubuntu/models:/home/ubuntu/models \
   -v /home/ubuntu/config:/home/ubuntu/config \
-  # Custom image with baked in model, see https://github.com/observIQ/application-helm-charts/tree/main/container/tensorflow
   ghcr.io/observiq/tensorflow:dc57387 \
   --monitoring_config_file=/home/ubuntu/config/monitoring_config.txt \
   --enable_batching \
