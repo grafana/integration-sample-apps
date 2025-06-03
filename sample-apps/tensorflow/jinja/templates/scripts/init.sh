841ce16f775874286cfc68e6ff578be44709b537
schmikei <keith.schmitty@gmail.com>
2025-06-03 13:20:17 -0400

    simplify installation and use pre-built container

--- sample-apps/tensorflow/jinja/templates/scripts/init.sh
+++ sample-apps/tensorflow/jinja/templates/scripts/init.sh
@@ -4,12 +4,11 @@ set -e
 
 # Run TensorFlow serving image
 docker run -d --platform linux/amd64 -p 8501:8501 --name tensorflow-serving --rm \
-  -e MODEL_NAME=${MODEL_NAME} \
+  -e MODEL_NAME=half_plus_two \
   -v /home/ubuntu/models:/home/ubuntu/models \
   -v /home/ubuntu/config:/home/ubuntu/config \
-  tensorflow/serving:2.19.0 \
-  --model_name=$MODEL_NAME \
-  --model_base_path=/home/ubuntu/models/$MODEL_NAME \
+  # Custom image with baked in model, see https://github.com/observIQ/application-helm-charts/tree/main/container/tensorflow
+  ghcr.io/observiq/tensorflow:dc57387 \
   --monitoring_config_file=/home/ubuntu/config/monitoring_config.txt \
   --enable_batching \
   --batching_parameters_file=/home/ubuntu/config/batching_config.txt
