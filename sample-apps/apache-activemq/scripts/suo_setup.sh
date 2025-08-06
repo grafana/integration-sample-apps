7375961ead0af83a73b3f0a14e07212d79220b1c
schmikei <keith.schmitty@gmail.com>
2025-08-06 10:25:24 -0400

    one more naming inconsistency

--- sample-apps/apache-activemq/scripts/suo_setup.sh
+++ sample-apps/apache-activemq/scripts/suo_setup.sh
@@ -11,14 +11,14 @@ sudo docker build -t sample/loadgenerator:0.0.1 .
 sudo docker image save sample/loadgenerator:0.0.1 -o /home/ubuntu/loadgen/loadgenerator.tar
 
 cd /home/ubuntu/configs
-sudo docker build -t sample/activemq:0.0.1 .
-sudo docker image save sample/activemq:0.0.1 -o /home/ubuntu/configs/activemq.tar
+sudo docker build -t sample/apache-activemq:0.0.1 .
+sudo docker image save sample/apache-activemq:0.0.1 -o /home/ubuntu/configs/activemq.tar
 
 sudo k3s ctr -n=k8s.io images import /home/ubuntu/loadgen/loadgenerator.tar
-sudo k3s ctr -n=k8s.io images import /home/ubuntu/configs/activemq.tar
+sudo k3s ctr -n=k8s.io images import /home/ubuntu/configs/apache-activemq.tar
 
 sudo rm /home/ubuntu/loadgen/loadgenerator.tar
-sudo rm /home/ubuntu/configs/activemq.tar
+sudo rm /home/ubuntu/configs/apache-activemq.tar
 
 kubectl create namespace apache-activemq --dry-run=client -o yaml | kubectl apply -f -
 helm install apache-activemq . --namespace apache-activemq \
